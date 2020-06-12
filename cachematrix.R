d089dd183919c6b3b50d7f3c2057ef1dfff2d486
Aharon Logue <aharon.logue@gmail.com>
2020-06-12 12:58:47 -0400

    Completed Assignment 2, and it even works!

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Here we have two functions, the first creating a list of functions, the second pulling from that list and inverting the matrix passed into the first one.
 
-## Write a short comment describing this function
+## This function initializes objects that store key info, and then defines its setters and getters
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      m <- NULL
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+      }
+      get <- function() x
+      setsolve <- function(solve) m <<- solve
+      getsolve <- function() m
+      list(set = set, get = get,
+           setsolve = setsolve,
+           getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## Then, pulling from the above function, this function does the solving. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      m <- x$getsolve()
+      if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+      }
+      data <- x$get()
+      m <- solve(data, ...)
+      x$setsolve(m)
+      m
 }
